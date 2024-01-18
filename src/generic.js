dfb76c83f7dc9bb6e352d36a6dc648b13a3c4aa0
Celso Fabri Junior <celso.fabri@live.com>
2024-01-18 15:00:14 -0300

    chore: update bg color address

--- src/generic.js
+++ src/generic.js
@@ -322,7 +322,7 @@ export const StyledAddress = styled.section`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 0.3);
+    background-color: rgba(255, 255, 255, 0.5);
     content: '';
   }
 `;
