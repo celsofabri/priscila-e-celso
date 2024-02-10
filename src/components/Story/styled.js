67dba5a6eb8b25e4acd0c7d2331cd7a18788ead3
Celso Fabri Junior <celso.fabri@live.com>
2024-02-10 15:19:37 -0300

    chore: update story styles

--- src/components/Story/styled.js
+++ src/components/Story/styled.js
@@ -31,8 +31,28 @@ export const StyledStorySubtitle = styled.h3`
 export const StyledStoryContent = styled.main`
   display: block;
 
+  h1 {
+    padding-left: 16px;
+    font-family: ${fonts.secondary};
+    font-size: 48px;
+    border-left: 5px solid ${colors.red500};
+  }
+
+  h2 {
+    color: ${colors.red500};
+  }
+
   img {
-    max-width: 100%;
+    display: block;
+    max-width: 80%;
     height: auto;
+    margin: 0 auto;
+  }
+
+  blockquote {
+    padding-left: 16px;
+    font-family: ${fonts.secondary};
+    font-size: 24px;
+    border-left: 5px solid ${colors.blue700};
   }
 `;
